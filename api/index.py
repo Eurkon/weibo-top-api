749eb148eaab74a270d68b65b04c68d3282d9bb5
Eurkon <1124724692@qq.com>
2021-06-03 10:16:57 +0800

    feat: 增加hot

--- api/index.py
+++ api/index.py
@@ -7,28 +7,22 @@ from http.server import BaseHTTPRequestHandler
 
 def get_data():
     news = []
-    # 新建数组存放热搜榜
     top_url = 'https://s.weibo.com/top/summary/'
-    # 热搜榜链接
     r = requests.get(top_url)
-    # 向链接发送get请求获得页面
-    soup = BeautifulSoup(r.text, 'html.parser')
-    # 解析页面
-    urls_titles = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > a')
-    hotness = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > span')
 
-    for i in range(len(urls_titles) - 1):
+    soup = BeautifulSoup(r.text, 'html.parser')
+    url = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > a')
+    num = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > span')
+    hot = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-03')
+
+    for i in range(1, len(url)):
         top_news = {}
-        top_news['rank'] = i + 1
-        # 将信息保存到字典中
-        top_news['title'] = urls_titles[i + 1].get_text()
-        # get_text()获得a标签的文本
-        top_news['url'] = "https://s.weibo.com" + urls_titles[i]['href']
-        # ['href']获得a标签的链接，并补全前缀
-        top_news['hotness'] = hotness[i].get_text()
-        # 获得热度文本
+        top_news['title'] = url[i].get_text()
+        top_news['url'] = "https://s.weibo.com" + url[i]['href']
+        top_news['num'] = num[i - 1].get_text()
+        top_news['hot'] = hot[i].get_text().replace('</i>', ''). \
+            replace('<i class="icon-txt icon-txt-hot">', '').replace('<i class="icon-txt icon-txt-new">', '')
         news.append(top_news)
-        # 字典追加到数组中
 
     return news
 
